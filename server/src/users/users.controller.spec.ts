af42ac8051c8f69f2a63d111a41f15c9917b903b
Marin Bareta <mbareta@extensionengine.com>
2024-06-18 17:19:05 +0200

    Add users controller test

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -5,6 +5,7 @@ import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +14,19 @@ describe('UsersController', () => {
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('works', async () => {
+      jest
+        .spyOn(service, 'findAll')
+        .mockImplementation(() => Promise.resolve([]));
+      expect(await controller.findAll()).toEqual([]);
+    });
   });
 });
