4f9c77fbb94a2a746311f31185c613c73545d91a
Marin Bareta <mbareta@extensionengine.com>
2024-07-23 13:56:45 +0200

    Remove Login component as it is not needed anymore

--- client/src/pages/admin/router.ts
+++ client/src/pages/admin/router.ts
@@ -3,12 +3,19 @@ import { createAuthGuard } from '@auth0/auth0-vue';
 import HomeView from './components/Home.vue';
 import UsersView from './components/users/index.vue';
 
-const routes = [
-  { path: '/', name: 'home', component: HomeView, beforeEnter: createAuthGuard() },
-  { path: '/users', name: 'users', component: UsersView, beforeEnter: createAuthGuard() }
-]
+const routes = [{
+  path: '/',
+  name: 'home',
+  component: HomeView,
+  beforeEnter: createAuthGuard()
+}, {
+  path: '/users',
+  name: 'users',
+  component: UsersView,
+  beforeEnter: createAuthGuard()
+}];
 
 export default createRouter({
   history: createWebHashHistory('/admin'),
   routes
-})
+});
