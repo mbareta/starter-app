7c4788aef3a344e66cf815b771b5a95a54e5006f
Marin Bareta <mbareta@extensionengine.com>
2024-08-13 15:57:22 +0200

    Add user's name to welcome message

--- client/src/pages/public/components/Home.vue
+++ client/src/pages/public/components/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <section class="hero is-success is-large">
       <div class="hero-body">
-        <h1 class="title">Welcome!</h1>
+        <h1 class="title">Welcome {{ $auth0.user.value.name }}!</h1>
         <h2 class="subtitle">There's no place like 127.0.0.1!</h2>
       </div>
     </section>
