8f880b5cda6dcf4b5d81f9c75631bc80020e23b7
Marin Bareta <mbareta@extensionengine.com>
2024-09-12 17:28:05 +0200

    Add Swagger OpenAPI docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
-import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import logger from './middleware/request-logger';
+import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -8,6 +9,16 @@ async function bootstrap() {
   app.use(logger);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Starter App')
+    .setDescription('Starter App API documentation')
+    .setVersion('1.0')
+    .addTag('starter_app')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
