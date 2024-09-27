b33a430fcbc6702c3ad68e827b82ccc2af16a1f4
Tomislav <tstipandzija@extensionengine.com>
2024-09-27 11:47:23 +0200

    Add tests

--- client/e2e/helpers/login.ts
+++ client/e2e/helpers/login.ts
@@ -1,4 +1,6 @@
-export async function login(page, email, password) {
+import { Page } from '@playwright/test';
+
+export async function login(page: Page, email: string, password: string): Promise<void> {
   await page.goto('/');
   await page.getByLabel('Email address').fill(email);
   await page.getByLabel('Password').fill(password);
