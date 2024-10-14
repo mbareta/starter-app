96c50ab012a27c3c5aabe86ac3797b6f5d69d3ea
Marin Bareta <mbareta@extensionengine.com>
2024-10-14 14:42:23 +0200

    Add API URL config value

--- client/src/pages/user/helpers/request.ts
+++ client/src/pages/user/helpers/request.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Instance of axios to be used for all API requests.
 const client = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
   headers: { 'Content-Type': 'application/json' }
 });
 
