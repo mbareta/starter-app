3532651c3553dca74e8a5bbf161d67769cfb93c2
Marin Bareta <mbareta@extensionengine.com>
2024-10-14 14:01:37 +0200

    Unify MikroORM config

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -4,19 +4,4 @@ import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === 'production';
-const ssl = isProduction
-  ? { ca: require('fs').readFileSync(`../../rds-cert-us-east-1.pem`) }
-  : false;
-
-export const DatabaseModule = MikroOrmModule.forRoot({
-  driver: PostgreSqlDriver,
-  driverOptions: { connection: { ssl } },
-  host: process.env.DATABASE_HOST,
-  port: +process.env.DATABASE_PORT,
-  dbName: process.env.DATABASE_NAME,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  entities: ['**/*.entity'],
-  autoLoadEntities: true
-});
+export const DatabaseModule = MikroOrmModule.forRoot();
