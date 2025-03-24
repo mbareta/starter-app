3bca440a5f077514b9c5dbc23636aa335170d0b1
Marin Bareta <mbareta@extensionengine.com>
2025-03-24 11:13:03 +0100

    Remove fastify server app (#154)

--- fast-server/logger.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  redact: ['body.password']
-}
--- fast-server/routes/root.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = async function (fastify, opts) {
-  fastify.get('/healthcheck', async function () {
-    return this.prisma.$queryRaw`SELECT 1`
-  })
-}
