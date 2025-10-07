8b03dda0b22e26f81b1078357af9295d67ac653e
Marin Bareta <mbareta@extensionengine.com>
2025-10-07 09:02:18 +0200

    Update Adobe authorize URL

--- client/src/pages/user/router.ts
+++ client/src/pages/user/router.ts
@@ -10,7 +10,7 @@ const createAuthGuard = () => {
     if (code) await useAuthStore().login(code);
     if (useAuthStore().isLoggedIn) return next();
     const adobeAuthorizeUrl =
-      'https://learningmanager.adobe.com/oauth/o/authorize';
+      'https://learningmanagereu.adobe.com/oauth/o/authorize';
     const adobeClientId = import.meta.env.VITE_ADOBE_CLIENT_ID;
     const adobeRedirectUri = import.meta.env.VITE_ADOBE_REDIRECT_URI;
     // eslint-disable-next-line max-len
